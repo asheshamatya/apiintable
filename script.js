bd98cd319e9cd5ab60bf775f3365584275bbcb18
Ashesh Amatya <86726654+asheshamatya@users.noreply.github.com>
2024-04-05 20:23:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 fetch("https://randomuser.me/api/?results=5").then(response => {
-    return response.json(); // Convert data to JSON
+    return response.json(); 
 }).then(data => {
     let tableData = "";
-    // Map over each user and create a row for the table
     data.results.map(user => {
         tableData += `<tr>
                         <td>${user.name.first}</td>
                         <td>${user.location.city}</td>
                     </tr>`;
     });
-    // Insert rows into the table body
     document.getElementById("table_body").innerHTML = tableData;
 });
